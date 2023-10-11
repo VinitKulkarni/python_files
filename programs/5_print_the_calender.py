e7e692360eb9f639fc1266fbcbedca3e8e212518
vinit <vinitkkulkarni@gmail.com>
2023-10-11 20:11:53 +0530

    Added:program to display the calender of the month and year specified

---
+++ programs/5_print_the_calender.py
@@ -0,0 +1,15 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-11 10:15:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to print the calendar of a given month and year.
+Note : Use 'calendar' module.
+'''
+
+import calendar
+
+year = 2023
+month = 10
+
+print(calendar.month(year, month))
