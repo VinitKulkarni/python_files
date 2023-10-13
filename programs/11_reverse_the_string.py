b64683ab3aecb056c6db307ff513ad333162199d
vinit <vinitkkulkarni@gmail.com>
2023-10-13 12:26:08 +0530

    Added:program to reverse the string

---
+++ programs/11_reverse_the_string.py
@@ -0,0 +1,12 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-12 16:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to reverse a string. 
+'''
+
+user_input = input("Enter the String: ")
+
+#reversing the string using slice
+print(user_input[::-1])
