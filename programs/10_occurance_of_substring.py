71f3e93b36de2111072ada232b93573e02b55f6e
vinit <vinitkkulkarni@gmail.com>
2023-10-13 12:23:42 +0530

    Added:program to get occurance of substring

---
+++ programs/10_occurance_of_substring.py
@@ -0,0 +1,13 @@
+'''
+@Author: Vinit 
+@Date: 2023-10-12 16:00:00
+@Last Modified by: None
+@Last Modified time: None
+@Title : Write a Python program to count occurrences of a substring in a string.
+'''
+
+#sample_data = "Write a Python program to count occurrences of a substring in a string"
+sample_data = "Python supports more built in libraries. Python is easy to learn"
+substring_data = 'Python'
+
+print(sample_data.count(substring_data))
